77f49ceed3676ff93e6c9e50aa1f9d4e72b949b9
Ashish Nishad <ashishnishad@Ashishs-MacBook-Air.local>
2025-01-08 01:08:34 +0530

    code push

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,25 +1,30 @@
-const getContact = (req, res) => {
-    res.status(200).json({ message: "Get all contacts" });
-};
+const asyncHandler = require("express-async-handler");
 
-const createContact = (req, res) => {
+
+const getContact = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Get all contacts" });
+});
+
+
+
+const createContact = asyncHandler(async(req, res) => {
     const { name, email, phone } = req.body;
     if (!name || !email || !phone) {
         return res.status(400).json({ message: "Please fill in all fields!" });
     }
     res.status(201).json({ message: "Contact created successfully!" });
-};
+});
 
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async(req, res) => {
     res.status(200).json({ message: `Update contact with ID: ${req.params.id}` });
-};
+});
 
-const getContactbyid = (req, res) => {
+const getContactbyid = asyncHandler(async(req, res) => {
     res.status(200).json({ message: `Get contact by ID: ${req.params.id}` });
-};
+});
 
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async(req, res) => {
     res.status(200).json({ message: `Delete contact with ID: ${req.params.id}` });
-};
+});
 
 module.exports = { getContact, createContact, updateContact, getContactbyid, deleteContact };
